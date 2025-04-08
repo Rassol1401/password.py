45744198031735062e1c7e7b4711a667e8e968e3
Rassol1401 <rassolenkomaksim1@gmail.com>
2025-04-08 18:24:07 +0300

    Add files via upload

--- password.py
+++ password.py
@@ -1,13 +1,10 @@
-password = input("Введите пароль: ")
-
-
 def has_digit(password):
     return any(bum.isdigit() for bum in password)
 
 
 def is_very_long(password):
-    if len(password) > 12:
-    	return True
+    return not len(password) >= 12
+
 
 
 def has_lower_letters(password):
@@ -22,22 +19,28 @@ def has_symbols(password):
     return any(not cek.isalnum() for cek in password)
 
 
-score = 0
+def main():
+    password = input("Введите пароль: ")
+    score = 0
 
-funktions = [
-    has_digit,
-    is_very_long,
-    has_lower_letters,
-    has_upper_letters,
-    has_symbols,
-]
+    funktions = [
+        has_digit,
+        is_very_long,
+        has_lower_letters,
+        has_upper_letters,
+        has_symbols,
+    ]
 
-for funkt in funktions:
-    if funkt(password):
-        score += 2
+    for funkt in funktions:
+        if funkt(password):
+            score += 2
 
 
-print("Рейтинг пароля:", score)
+    print("Рейтинг пароля:", score)
+
+
+if __name__ == '__main__':
+    main()    
 
 
 
